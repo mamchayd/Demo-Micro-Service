6fa35c5040f74a01f40e4e6f5e83c81e409a6a50
Amchayd Mariam <meriam.amchayd@gmail.com>
2020-11-23 22:10:41 +0100

    Rest controller

--- src/main/java/ma/enset/DemoMicroServiceApplication.java
+++ src/main/java/ma/enset/DemoMicroServiceApplication.java
@@ -1,15 +1,25 @@
 package ma.enset;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -55,5 +65,48 @@ class Produit{
 interface ProduitRepository extends JpaRepository<Produit, Long>{
 	
 }
+
+@RestController
+class ProduitRestController{
+	
+	@Autowired
+	private ProduitRepository produitRepository;
+	
+	@GetMapping(path = "/produits")
+	public List<Produit> list(){
+		return produitRepository.findAll();
+	}
+	
+	@GetMapping(path = "/produits/{id}")
+	public Produit getOne(@PathVariable Long id){
+		return produitRepository.findById(id).get();
+	}
+	
+	@PostMapping(path = "/produits}")
+	public Produit save (@RequestBody Produit produit){
+		return produitRepository.save(produit);
+	}
+	
+	@PutMapping(path = "/produits/{id}")
+	public Produit update (@PathVariable Long id,@RequestBody Produit produit){
+		produit.setId(id);
+		return produitRepository.save(produit);
+	}
+	
+	@DeleteMapping(path = "/produits/{id}")
+	public void delete (@PathVariable Long id){
+		produitRepository.deleteById(id);
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
 
 
